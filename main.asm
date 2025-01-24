fb3ef699552799d7c70ffeab2eefb9715168fd04
Michał Pociecha-Łoś <micapolos@gmail.com>
2025-01-24 17:02:38 +0100

    tilemap text mode

--- main.asm
+++ main.asm
@@ -6,7 +6,6 @@ CHAR_COUNT  EQU   96
 
         INCLUDE blit.asm
         INCLUDE surface.asm
-        INCLUDE tiledef.asm
 
 screenSurface:
         Surface { tileMap, 80, 32 }
@@ -20,14 +19,13 @@ main:
         nextreg $07, 3  ; 28MHz clock
         nextreg $54, 34  ; MMU 4, 34
 
-        nextreg $6b, %11000011  ; enable tilemap, 80x32, 512 tiles, tilemap over ULA
+        nextreg $6b, %11001011  ; enable tilemap, 80x32, 512 tiles, textmode, tilemap over ULA
         nextreg $6c, %00000000  ; Default tilemap attribute
         nextreg $6e, (tileMap - $4000) >> 8
         nextreg $6f, (tileDefs - $4000) >> 8
         nextreg $68, %10000000  ; Disable ULA output
 
         call    InitTilemapPalette
-        call    InitTileDefs
 
         ld      ix, screenSurface
         ld      iy, backSurface
@@ -62,46 +60,22 @@ InitTilemapPalette:
         djnz    .loop
         ret
 
-InitTileDefs
-        ld      hl, tileDefs
-        ld      de, fontBitmap
-        ld      bc, $6020  ; count / color (foreground/background)
-.loop
-        call    Tiledef.ConvertBitmap
-        djnz    .loop
-
         ORG $4000
 
 tileMap:
         ds      80 * 32 * 2
 
 tileDefs:
+        INCLUDE topaz-8.asm
         ds      96 * 32
 
         MMU     4, 34
         ORG     $8000
 
 tilemapPalette:
-        DUP 32
-        ; dimmed colors
+        DUP 128
         DB %00000000
-        DB %00000010
         DB %00010000
-        DB %00010010
-        DB %10000000
-        DB %10000010
-        DB %10010000
-        DB %10010010
-
-        ; full colors
-        DB %00000000
-        DB %00000011
-        DB %00011100
-        DB %00011111
-        DB %11100000
-        DB %11100011
-        DB %11111100
-        DB %11111111
         EDUP
 
 fontBitmap:
