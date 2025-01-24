2367aa3cb581bbf413baacd477233845ed043c64
Michał Pociecha-Łoś <micapolos@gmail.com>
2025-01-24 14:07:35 +0100

    reformat

--- blit.asm
+++ blit.asm
@@ -1,7 +1,7 @@
-				IFNDEF  BLIT_LIB
-				DEFINE 	BLIT_LIB
+        IFNDEF  BLIT_LIB
+        DEFINE  BLIT_LIB
 
-				MODULE 	Blit
+        MODULE  Blit
 
 ; Input:
 ;   hl - addr
@@ -16,8 +16,8 @@ Put16:
         ret
 
 ; Input:
-;	  hl - address
-;		bc - width, height
+;   hl - address
+;   bc - width, height
 ;   de - value
 ;   a - stride
 ; Output:
@@ -25,22 +25,22 @@ Put16:
 ;   bc - width, 0
 FillRect16:
 .loop
-				push	bc
+        push    bc
 .rowLoop
-				ld 		(hl), e
-				inc 	hl
-				ld    (hl), d
-				inc   hl
-				djnz	.rowLoop
+        ld      (hl), e
+        inc     hl
+        ld      (hl), d
+        inc     hl
+        djnz    .rowLoop
 
-				add		hl, a
-				add		hl, a
+        add     hl, a
+        add     hl, a
 
-				pop   bc
-				dec		c
-				jp		nz, .loop
+        pop     bc
+        dec     c
+        jp      nz, .loop
 
-				ret
+        ret
 
 ; Input:
 ;   HL - src start
@@ -53,28 +53,28 @@ FillRect16:
 ;   DE - dst end
 ;   BC - ?
 CopyRect8:
-				push 	af
+        push    af
 .nextRow
-				push 	bc
-				ld 	 	a, b
+        push    bc
+        ld      a, b
 .nextCell
-				ldi
-				inc  	bc
-				djnz 	.nextCell
-				pop	 	bc
-				ld 		b, a
+        ldi
+        inc     bc
+        djnz    .nextCell
+        pop     bc
+        ld      b, a
 
-				ld    a, ixh
-				add   hl, a
-				ld    a, ixl
-				add   de, a
+        ld      a, ixh
+        add     hl, a
+        ld      a, ixl
+        add     de, a
 
-				dec 	c
-	      jp 		nz, .nextRow
+        dec     c
+        jp      nz, .nextRow
 
-				pop 	af
-				ret
+        pop     af
+        ret
 
-				ENDMODULE
+        ENDMODULE
 
-				ENDIF
+        ENDIF
