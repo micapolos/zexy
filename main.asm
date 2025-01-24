2238166855ceb5c33396ec104c863e15f6fb8ec5
Michał Pociecha-Łoś <micapolos@gmail.com>
2025-01-24 03:52:25 +0100

    Surface FillRect/CopyRect

--- main.asm
+++ main.asm
@@ -1,115 +1,91 @@
-  DEVICE ZXSPECTRUMNEXT
-
-BAR EQU 384
+        DEVICE ZXSPECTRUMNEXT
 
 CHAR_COUNT  EQU   96
 
-  ORG $c000
+        ORG $c000
 
-  INCLUDE blit.asm
-  INCLUDE surface.asm
+        INCLUDE blit.asm
+        INCLUDE surface.asm
 
 screenSurface   Surface { 32, 80, tileMap }
 backSurface     Surface { 32, 80, backTileMap }
 
 main:
-  di
+        di
 
-  nextreg $07, 3  ; 28MHz clock
-  nextreg $54,34  ; MMU 4, 34
+        nextreg $07, 3  ; 28MHz clock
+        nextreg $54, 34  ; MMU 4, 34
 
-  nextreg $6b, %11000011  ; enable tilemap, 80x32, 512 tiles, tilemap over ULA
-  nextreg $6c, %00000000  ; Default tilemap attribute
-  nextreg $6e, (tileMap - $4000) >> 8
-  nextreg $6f, (tileDefs - $4000) >> 8
-  nextreg $68, %10000000  ; Disable ULA output
+        nextreg $6b, %11000011  ; enable tilemap, 80x32, 512 tiles, tilemap over ULA
+        nextreg $6c, %00000000  ; Default tilemap attribute
+        nextreg $6e, (tileMap - $4000) >> 8
+        nextreg $6f, (tileDefs - $4000) >> 8
+        nextreg $68, %10000000  ; Disable ULA output
 
-  call  copyTilemapPalette
+        call    copyTilemapPalette
 
-  ld    ix, screenSurface
-  ld    iy, backSurface
+        ld      ix, screenSurface
+        ld      iy, backSurface
 
-  ld    hl, $0000     ; col / row
-  ld    bc, $5020     ; width / height
-  ld    de, $0001     ; value
-  call  Surface.FillRect
+        ld      hl, $0000     ; col / row
+        ld      bc, $5020     ; width / height
+        ld      de, $0001     ; value
+        call    Surface.FillRect
 
-  ld    hl, $1008     ; col / row
-  ld    bc, $2008     ; width / height
-  ld    de, $0010     ; tile
-  call  Surface.FillRect
+        ld      hl, $1008     ; col / row
+        ld      bc, $2008     ; width / height
+        ld      de, $0010     ; tile
+        call    Surface.FillRect
 
-  ld    hl, $0000     ; src col / row
-  ld    de, $0000     ; dst col / row
-  ld    bc, $1004     ; width / height
-  call  Surface.CopyRect
+        ld      hl, $0000     ; src col / row
+        ld      de, $0000     ; dst col / row
+        ld      bc, $1004     ; width / height
+        call    Surface.CopyRect
 
-  ; start with yellow bar
-  ld a,6
-.loop:
-  out (254),a
-  xor 7
-  ld bc,BAR
-  call wait
-  jp .loop
-
-wait:
-  push af
-.loop:
-  dec bc
-  ld a,b
-  or c
-  jr nz,.loop
-
-  pop af
-  ret
+.loop:  jp      .loop
 
 copyTilemapPalette:
-  nextreg $43, %00110000 ; tilemap 1-st palette for write, auto-increment
-  nextreg $40, 0  ; start palette index = 0
-  ld  hl, tilemapPalette
-        ld  b, 0
+        nextreg $43, %00110000 ; tilemap 1-st palette for write, auto-increment
+        nextreg $40, 0  ; start palette index = 0
+        ld      hl, tilemapPalette
+        ld      b, 0
 .loop:
-      ld  a, (hl)
-  inc   hl
+        ld      a, (hl)
+        inc     hl
         nextreg $41, a  ; write palette color
-        djnz  .loop
+        djnz    .loop
         ret
 
-  ORG $4000
+        ORG $4000
 
 tileMap:
-  ds      80*32*2
+        ds      80*32*2
 
 tileDefs:
-  INCLUDE topaz.asm
-
-  MMU   4, 34, $8000
-scr:
-  INCBIN "RoboCop.scr"
+        INCLUDE topaz.asm
 
 tilemapPalette:
-  DUP 32
-  ; dimmed colors
-  DB %00000000
-  DB %00000010
-  DB %00010000
-  DB %00010010
-  DB %10000000
-  DB %10000010
-  DB %10010000
-  DB %10010010
+        DUP 32
+        ; dimmed colors
+        DB %00000000
+        DB %00000010
+        DB %00010000
+        DB %00010010
+        DB %10000000
+        DB %10000010
+        DB %10010000
+        DB %10010010
 
-  ; full colors
-  DB %00000000
-  DB %00000011
-  DB %00011100
-  DB %00011111
-  DB %11100000
-  DB %11100011
-  DB %11111100
-  DB %11111111
-  EDUP
+        ; full colors
+        DB %00000000
+        DB %00000011
+        DB %00011100
+        DB %00011111
+        DB %11100000
+        DB %11100011
+        DB %11111100
+        DB %11111111
+        EDUP
 
 backTileMap:
         DUP   80*32, i
@@ -117,6 +93,6 @@ backTileMap:
         DB    ((i % CHAR_COUNT) & $100) >> 8
         EDUP
 
-  SAVENEX OPEN "main.nex", main, $FFFE
-  SAVENEX AUTO
-      SAVENEX CLOSE
+        SAVENEX OPEN "main.nex", main, $FFFE
+        SAVENEX AUTO
+        SAVENEX CLOSE
