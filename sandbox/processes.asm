187c84c2304fa60744f215f56718a036085072dc
Michał Pociecha-Łoś <micapolos@gmail.com>
2025-01-31 01:40:12 +0100

    scheduler

--- sandbox/processes.asm
+++ sandbox/processes.asm
@@ -8,7 +8,6 @@
 
 Main
         nextreg NextReg.ULA_CONTROL, %10000000  ; disable ula
-        nextreg NextReg.CPU_SPEED, 0   ; 28MHz
 
         call    Scheduler.Init
 
@@ -25,6 +24,26 @@ Main
         ld      bc, Process.entry
         ld      de, Process.stack3
         ld      a, $e7
+        call    Scheduler.Launch
+
+        ld      bc, Process.entry
+        ld      de, Process.stack4
+        ld      a, $27
+        call    Scheduler.Launch
+
+        ld      bc, Process.entry
+        ld      de, Process.stack5
+        ld      a, $d7
+        call    Scheduler.Launch
+
+        ld      bc, Process.entry
+        ld      de, Process.stack6
+        ld      a, $3b
+        call    Scheduler.Launch
+
+        ld      bc, Process.entry
+        ld      de, Process.stack7
+        ld      a, $f1
         call    Scheduler.Launch
 
         xor     a
@@ -45,6 +64,16 @@ Process
 .stack2
         ds      $100
 .stack3
+        ds      $100
+.stack4
+        ds      $100
+.stack5
+        ds      $100
+.stack6
+        ds      $100
+.stack7
+        ds      $100
+.stack8
 
         savenex open "sandbox/processes.nex", Main, $bfe0
         savenex auto
