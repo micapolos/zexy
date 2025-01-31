3f302989a7778bdfb10215905fab83398deb771e
Michał Pociecha-Łoś <micapolos@gmail.com>
2025-01-31 09:06:54 +0100

    DMA sound

--- sandbox/dma-sound.asm
+++ sandbox/dma-sound.asm
@@ -5,6 +5,8 @@
         include nextreg.asm
 
 Main
+        nextreg NextReg.ULA_CONTROL, %10000000  ; disable ula
+
         ; Fill triangle wave, 256 bytes
         xor     a
         ld      hl, buffer
@@ -31,16 +33,20 @@ Main
         ld      c, $6b
         otir
 
-.loop   jp      .loop
+.loop
+        inc     a
+        nextreg NextReg.TRANS_COLOR_FALLBACK, a
+        jp      .loop
 
 dmaProgram
         db      %10000011       ; WR6: disable DMA
         db      %01111101       ; WR0: A->B, transfer
         dw      buffer          ; start address A
         dw      buffer.size     ; length
-        db      %00010100       ; WR1: port A increment, memory
+        db      %01010100       ; WR1: port A increment, memory
+        db      %00000010       ; cycle length 2
         db      %01111000       ; WR2: port B static, I/O
-        db      %00100000       ; cycle length 4, next byte prescalar
+        db      %00100010       ; cycle length 2, next byte prescalar
         db      %00001000       ; prescalar
         db      %11001101       ; WR4: burst mode
         dw      $dfdf           ; start address B (DAC)
@@ -52,7 +58,7 @@ dmaProgram
 buffer  ds      .size
 .size   equ     $100
 
-        cspectmap "sandbox/dma-sound.map"
+        cspectmap    "sandbox/dma-sound.map"
         savenex open "sandbox/dma-sound.nex", Main, $bfe0
         savenex auto
         savenex close
