dc6addab7708487184e74778a963f989ae4c8744
Michał Pociecha-Łoś <micapolos@gmail.com>
2025-02-03 08:25:44 +0100

    dma

--- dma.asm
+++ dma.asm
@@ -11,25 +11,51 @@
 ;   bc - size
 ;   a - value
 Fill
-        ld      hl, .startA
-        ld      (hl), .value
-
-        ld      hl, .len
+        ld      hl, program.len
         ld      (hl), bc
 
-        ld      hl, .startB
+        ld      hl, program.startB
         ld      (hl), de
 
-        ld      (.value), a
+        ld      hl, program.startA
+        ld      bc, program.value
+        ld      (hl), bc
 
-        ld      hl, .program
-        ld      b, .size
+        ld      (program.value), a
+
+        ld      hl, program
+        ld      b, program.size
         ld      c, Port.DMA
         otir
 
         ret
 
-.program
+; =========================================================
+; Input
+;   de - dst
+;   bc - size
+;   a - value
+Copy
+        ld      hl, program.startA
+        ld      (hl), program.value
+
+        ld      hl, program.len
+        ld      (hl), bc
+
+        ld      hl, program.startB
+        ld      (hl), de
+
+        ld      (program.value), a
+
+        ld      hl, program
+        ld      b, program.size
+        ld      c, Port.DMA
+        otir
+
+        ret
+
+; =========================================================
+program
         db      %10000011       ; WR6: disable DMA
         db      %01111101       ; WR0: A->B, transfer
 .startA dw      0               ; start address A
@@ -41,7 +67,7 @@ Fill
         db      %10000010       ; WR5: stop on end
         db      %11001111       ; WR6: load
         db      %10000111       ; WR6: enable DMA
-.size   equ     $ - .program
+.size   equ     $ - program
 .value  db      0
 
         endmodule
