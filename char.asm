d89f74aa60aaa05ed69ed728d39177c08e3c11d4
Michał Pociecha-Łoś <micapolos@gmail.com>
2025-02-04 07:44:18 +0100

    delete

--- char.asm
+++ char.asm
@@ -7,6 +7,7 @@ backSpace       equ      $08
 newLine         equ      $0a
 space           equ      ' '
 quotes          equ      '"'
+quote           equ      $27
 delete          equ      $7f
 
         endmodule
