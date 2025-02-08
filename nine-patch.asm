15affc4a2a4ead1b75237e4fb815101800fcbbc3
Michał Pociecha-Łoś <micapolos@gmail.com>
2025-02-08 18:25:43 +0100

    nine-patch

--- nine-patch.asm
+++ nine-patch.asm
@@ -4,35 +4,18 @@
 ; TODO: add support for transparent color
 
         struct  NinePatch
-; data address
 addr                    dw
 
-; bit 7: left enabled
-; bit 6: middle enabled
-; bit 5: middle transparent
-; bit 4: right enabled
-; bit 3: top enabled
-; bit 2: middle enabled
-; bit 1: transparent color enabled (not yet supported)
-; bit 0: bottom enabled
+; bit 1: 1 = enable transparent color, 0 = no transparent color
+; bit 0: 1 = middle opaque, 0 = middle transparent
 flags                   db
-
-; bit 7..4: left width, 0 = 16
-; bit 3..0: right width, 0 = 16
-leftRightWidths         db
-
-; middle width, 0 = 65536
-middleWidth             dw
-
-; bit 7..4: top height, 0 = 16
-; bit 3..0: bottom height, 0 = 16
-topBottomHeights        db
-
-; middle height, 0 = 65536
-middleHeight            dw
-
-; transparent color index (not yet supported)
 transparentColor        db
+leftWidth               dw
+middleWidth             dw
+rightWidth              dw
+topHeight               dw
+middleHeight            dw
+bottomHeight            dw
         ends
 
         assert (NinePatch & 1) = 0
