08c6c40154653d646a7d0ce2ab21562b712fbe7f
Michał Pociecha-Łoś <micapolos@gmail.com>
2025-05-20 22:21:02 +0200

    bank alloc / free

--- nex/bank-demo.asm
+++ nex/bank-demo.asm
@@ -11,8 +11,27 @@ Main
 
         call    TotalBanks
         call    AvailableBanks
+
         call    AllocateBank
+        ld      (bank1), a
+
         call    AvailableBanks
+
+        call    AllocateBank
+        ld      (bank2), a
+
+        call    AvailableBanks
+
+        ld      a, (bank2)
+        call    FreeBank
+        call    AvailableBanks
+
+        ld      a, (bank1)
+        call    FreeBank
+        call    AvailableBanks
+
+        WritelnString "Goodbye!!!"
+
         jp      End
 
 TotalBanks
@@ -35,17 +54,40 @@ AllocateBank
         WriteString "Allocating bank... "
         call    Bank.Alloc
         jp      c, Error
+        ld      a, e
 
-        push    de
+        push    af
         WriteString "done: "
         pop     af
+        push    af
         call    Writer.Hex8h
-        jp      Writer.NewLine
+        call    Writer.NewLine
+        pop     af
+        ret
+
+FreeBank
+        push    af
+        WriteString "Freeing bank: "
+        pop     af
+        push    af
+        call    Writer.Hex8h
+        WriteString "... "
+        pop     af
+
+        ld      e, a
+        call    Bank.Free
+        jp      c, Error
+
+        WritelnString "OK"
+        ret
 
 Error
         WritelnString "Error!!!"
 
 End     jp      End
+
+@bank1   db      0
+@bank2   db      0
 
         savenex open "built/bank-demo.nex", Main, $bfe0
         savenex auto
