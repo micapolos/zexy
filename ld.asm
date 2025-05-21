0c8f6708140bd62eb4acbfa3282977654add8e41
Michał Pociecha-Łoś <micapolos@gmail.com>
2025-05-22 00:03:29 +0200

    dot

--- ld.asm
+++ ld.asm
@@ -3,6 +3,11 @@
 
         ; === ld ===
 
+        macro   ld_cde_u24 u24
+        ld      c, (u24) >> 16
+        ld      de, (u24) & $ffff
+        endm
+
         macro   ld_bcde_u32 u32
         ld      bc, (u32) >> 16
         ld      de, (u32) & $ffff
@@ -20,6 +25,11 @@
         ldi_ihl_u8 (u16) >> 8
         endm
 
+        macro   ldi_ihl_u24 u24
+        ldi_ihl_u16 (u24) & $ffff
+        ldi_ihl_u8 (u24) >> 16
+        endm
+
         macro   ldi_ihl_u32 u32
         ldi_ihl_u16 (u32) & $ffff
         ldi_ihl_u16 (u32) >> 16
@@ -34,6 +44,15 @@
         ld      (hl), e
         inc     hl
         ld      (hl), d
+        inc     hl
+        endm
+
+        macro   ldi_ihl_cde
+        ld      (hl), e
+        inc     hl
+        ld      (hl), d
+        inc     hl
+        ld      (hl), c
         inc     hl
         endm
 
@@ -60,6 +79,15 @@
         inc     hl
         endm
 
+        macro   ldi_cde_ihl
+        ld      e, (hl)
+        inc     hl
+        ld      d, (hl)
+        inc     hl
+        ld      c, (hl)
+        inc     hl
+        endm
+
         macro   ldi_bcde_ihl
         ld      e, (hl)
         inc     hl
@@ -83,6 +111,11 @@
         ldd_ihl_u8 (u16) & $ff
         endm
 
+        macro   ldd_ihl_u24 u24
+        ldd_ihl_u8 (u24) >> 16
+        ldd_ihl_u16 (u24) & $ffff
+        endm
+
         macro   ldd_ihl_u32 u32
         ldd_ihl_u16 (u32) >> 16
         ldd_ihl_u16 (u32) & $ffff
@@ -94,6 +127,15 @@
         endm
 
         macro   ldd_ihl_de
+        dec     hl
+        ld      (hl), d
+        dec     hl
+        ld      (hl), e
+        endm
+
+        macro   ldd_ihl_cde
+        dec     hl
+        ld      (hl), c
         dec     hl
         ld      (hl), d
         dec     hl
@@ -117,6 +159,15 @@
         endm
 
         macro   ldd_de_ihl
+        dec     hl
+        ld      d, (hl)
+        dec     hl
+        ld      e, (hl)
+        endm
+
+        macro   ldd_cde_ihl
+        dec     hl
+        ld      c, (hl)
         dec     hl
         ld      d, (hl)
         dec     hl
--- nex/ld-demo.asm
+++ nex/ld-demo.asm
@@ -12,10 +12,12 @@ Main
         ld      hl, data
         ldi_ihl_u8  $01
         ldi_ihl_u16 $0302
-        ldi_ihl_u32 $07060504
+        ldi_ihl_u24 $060504
+        ldi_ihl_u32 $0a090807
 
-        ld      hl, data2 + 7
-        ldd_ihl_u32 $07060504
+        ld      hl, data2 + 10
+        ldd_ihl_u32 $0a090807
+        ldd_ihl_u24 $060504
         ldd_ihl_u16 $0302
         ldd_ihl_u8  $01
 
@@ -24,24 +26,30 @@ Main
         ldi_ihl_a
         ld      de, $0302
         ldi_ihl_de
-        ld_bcde_u32 $07060504
+        ld_cde_u24  $060504
+        ldi_ihl_cde
+        ld_bcde_u32 $0a090807
         ldi_ihl_bcde
 
-        ld      hl, data4 + 7
-        ld_bcde_u32 $07060504
+        ld      hl, data4 + 10
+        ld_bcde_u32 $0a090807
         ldd_ihl_bcde
+        ld_cde_u24  $060504
+        ldd_ihl_cde
         ld      de, $0302
         ldd_ihl_de
-        ld      a, $01
+        ld      a,  $01
         ldd_ihl_a
 
         ld      hl, data5
         ldi_ihl_u8  $01       : ldd_a_ihl    : inc a : ldi_ihl_a
         ldi_ihl_u16 $0302     : ldd_de_ihl   : inc d : inc e : ldi_ihl_de
-        ldi_ihl_u32 $07060504 : ldd_bcde_ihl : inc b : inc c : inc d : inc e : ldi_ihl_bcde
+        ldi_ihl_u24 $060504   : ldd_cde_ihl  : inc c : inc d : inc e : ldi_ihl_cde
+        ldi_ihl_u32 $0a090807 : ldd_bcde_ihl : inc b : inc c : inc d : inc e : ldi_ihl_bcde
 
-        ld      hl, data6 + 7
-        ldd_ihl_u32 $07060504 : ldi_bcde_ihl : inc b : inc c : inc d : inc e : ldd_ihl_bcde
+        ld      hl, data6 + 10
+        ldd_ihl_u32 $0a090807 : ldi_bcde_ihl : inc b : inc c : inc d : inc e : ldd_ihl_bcde
+        ldd_ihl_u24 $060504   : ldi_cde_ihl  : inc c : inc d : inc e : ldd_ihl_cde
         ldd_ihl_u16 $0302     : ldi_de_ihl   : inc d : inc e : ldd_ihl_de
         ldd_ihl_u8  $01       : ldi_a_ihl    : inc a : ldd_ihl_a
 
@@ -49,12 +57,12 @@ Main
 
 .end    jp      .end
 
-data    block 8, 0
-data2   block 8, 0
-data3   block 8, 0
-data4   block 8, 0
-data5   block 8, 0
-data6   block 8, 0
+data    block 16, 0
+data2   block 16, 0
+data3   block 16, 0
+data4   block 16, 0
+data5   block 16, 0
+data6   block 16, 0
 
         savenex open "built/ld-demo.nex", Main, $bfe0
         savenex auto
