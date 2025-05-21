0c51f616849f9b6ec4a5bcba458f1d6b4d4d2a5b
Michał Pociecha-Łoś <micapolos@gmail.com>
2025-05-21 21:04:05 +0200

    dot

--- ld.asm
+++ ld.asm
@@ -1,21 +1,28 @@
         ifndef  LD_asm
         define  LD_asm
 
+        ; === ld ===
+
+        macro   ld_bcde_u32 u32
+        ld      bc, (u32) >> 16
+        ld      de, (u32) & $ffff
+        endm
+
         ; === ldi ===
 
         macro   ldi_ihl_u8 u8
-        ld      (hl), u8
+        ld      (hl), (u8)
         inc     hl
         endm
 
         macro   ldi_ihl_u16 u16
-        ldi_ihl_u8 (u16 & $ff)
-        ldi_ihl_u8 (u16 >> 8)
+        ldi_ihl_u8 (u16) & $ff
+        ldi_ihl_u8 (u16) >> 8
         endm
 
         macro   ldi_ihl_u32 u32
-        ldi_ihl_u16 (u32 & $ffff)
-        ldi_ihl_u16 (u32 >> 16)
+        ldi_ihl_u16 (u32) & $ffff
+        ldi_ihl_u16 (u32) >> 16
         endm
 
         macro   ldi_ihl_a
@@ -72,13 +79,13 @@
         endm
 
         macro   ldd_ihl_u16 u16
-        ldd_ihl_u8 (u16 >> 8)
-        ldd_ihl_u8 (u16 & $ff)
+        ldd_ihl_u8 (u16) >> 8
+        ldd_ihl_u8 (u16) & $ff
         endm
 
         macro   ldd_ihl_u32 u32
-        ldd_ihl_u16 (u32 >> 16)
-        ldd_ihl_u16 (u32 & $ffff)
+        ldd_ihl_u16 (u32) >> 16
+        ldd_ihl_u16 (u32) & $ffff
         endm
 
         macro   ldd_ihl_a
--- nex/ld-demo.asm
+++ nex/ld-demo.asm
@@ -24,13 +24,11 @@ Main
         ldi_ihl_a
         ld      de, $0302
         ldi_ihl_de
-        ld      bc, $0706
-        ld      de, $0504
+        ld_bcde_u32 $07060504
         ldi_ihl_bcde
 
         ld      hl, data4 + 7
-        ld      bc, $0706
-        ld      de, $0504
+        ld_bcde_u32 $07060504
         ldd_ihl_bcde
         ld      de, $0302
         ldd_ihl_de
@@ -41,6 +39,11 @@ Main
         ldi_ihl_u8  $01       : ldd_a_ihl    : inc a : ldi_ihl_a
         ldi_ihl_u16 $0302     : ldd_de_ihl   : inc d : inc e : ldi_ihl_de
         ldi_ihl_u32 $07060504 : ldd_bcde_ihl : inc b : inc c : inc d : inc e : ldi_ihl_bcde
+
+        ld      hl, data6 + 7
+        ldd_ihl_u32 $07060504 : ldi_bcde_ihl : inc b : inc c : inc d : inc e : ldd_ihl_bcde
+        ldd_ihl_u16 $0302     : ldi_de_ihl   : inc d : inc e : ldd_ihl_de
+        ldd_ihl_u8  $01       : ldi_a_ihl    : inc a : ldd_ihl_a
 
         break
 
