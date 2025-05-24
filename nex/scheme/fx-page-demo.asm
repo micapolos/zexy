cbe4634ac50eecae151e56b676837b62b81fb917
Michał Pociecha-Łoś <micapolos@gmail.com>
2025-05-24 19:15:20 +0200

    fx-page

--- nex/scheme/fx-page-demo.asm
+++ nex/scheme/fx-page-demo.asm
@@ -1,105 +1,98 @@
-  device  zxspectrumnext
-
-  org     $8000
-
-  include terminal.asm
-  include writer.asm
-  include ld.asm
-  include dump.asm
   include control.asm
-  include scheme/fx-page.asm
-  include mem.asm
 
-  _const SEGMENT_BIT_SIZE, 3
-  _const SEGMENT_SIZE, 1 << (SEGMENT_BIT_SIZE + 1)
-  _const SEGMENT_ADDR_MASK, SEGMENT_SIZE - 1
+  _nex fx-page-demo
+    include terminal.asm
+    include writer.asm
+    include ld.asm
+    include dump.asm
+    include control.asm
+    include scheme/fx-page.asm
+    include mem.asm
 
-  macro   PressSpaceTo msg
-    WriteString "<- Press SPACE to "
-    WriteString msg
-    WritelnString " ->"
-    call    Debug.WaitSpace
-    call    Writer.NewLine
-  endm
+    _const SEGMENT_BIT_SIZE, 3
+    _const SEGMENT_SIZE, 1 << (SEGMENT_BIT_SIZE + 1)
+    _const SEGMENT_ADDR_MASK, SEGMENT_SIZE - 1
 
-  _proc Main
-    call    Terminal.Init
+    macro PressSpaceTo msg
+      WriteString "<- Press SPACE to "
+      WriteString msg
+      WritelnString " ->"
+      call    Debug.WaitSpace
+      call    Writer.NewLine
+    endm
 
-    WriteString "Segment bit size: "
-    ld      a, SEGMENT_BIT_SIZE
-    call    Writer.Hex8
-    call    Writer.NewLine
+    _main
+      call    Terminal.Init
 
-    WriteString "Segment size: "
-    ld      hl, SEGMENT_SIZE
-    call    Writer.Hex16
-    call    Writer.NewLine
+      WriteString "Segment bit size: "
+      ld      a, SEGMENT_BIT_SIZE
+      call    Writer.Hex8
+      call    Writer.NewLine
 
-    WriteString "Segment mask: "
-    ld      hl, SEGMENT_ADDR_MASK
-    call    Writer.Hex16
-    call    Writer.NewLine
+      WriteString "Segment size: "
+      ld      hl, SEGMENT_SIZE
+      call    Writer.Hex16
+      call    Writer.NewLine
 
-    call    Writer.NewLine
+      WriteString "Segment mask: "
+      ld      hl, SEGMENT_ADDR_MASK
+      call    Writer.Hex16
+      call    Writer.NewLine
 
-    ; Initialize segment config
-    ld      hl, Segment
-    ldi_ihl_u16 SEGMENT_ADDR_MASK
-    ldi_ihl_u8  SEGMENT_BIT_SIZE
+      call    Writer.NewLine
 
-    call    Reset
-    WritelnString "Init size 3"
-    ld      hl, segment
-    ld      a, 3
-    call    FxPage.Init
-    call    Dump
+      ; Initialize segment config
+      ld      hl, Segment
+      ldi_ihl_u16 SEGMENT_ADDR_MASK
+      ldi_ihl_u8  SEGMENT_BIT_SIZE
 
-    call    Reset
-    WritelnString "Init size 2"
-    ld      hl, segment
-    ld      a, 2
-    call    FxPage.Init
-    call    Dump
+      call    Reset
+      WritelnString "Init size 3"
+      ld      hl, segment
+      ld      a, 3
+      call    FxPage.Init
+      call    Dump
 
-    call    Reset
-    WritelnString "Init size 1"
-    ld      hl, segment
-    ld      a, 1
-    call    FxPage.Init
-    call    Dump
+      call    Reset
+      WritelnString "Init size 2"
+      ld      hl, segment
+      ld      a, 2
+      call    FxPage.Init
+      call    Dump
 
-    call    Reset
-    WritelnString "Init size 0"
-    ld      hl, segment
-    ld      a, 0
-    call    FxPage.Init
-    call    Dump
+      call    Reset
+      WritelnString "Init size 1"
+      ld      hl, segment
+      ld      a, 1
+      call    FxPage.Init
+      call    Dump
 
-    _block end
-      jp .end
+      call    Reset
+      WritelnString "Init size 0"
+      ld      hl, segment
+      ld      a, 0
+      call    FxPage.Init
+      call    Dump
+
+      _freeze
+    _end
+
+    _proc Dump
+      ld      hl, segment
+      ld      bc, SEGMENT_SIZE
+      call    Writer.Dump
+      call    Writer.NewLine
+    _end
+
+    _proc Reset
+      ld      hl, segment
+      ld      bc, SEGMENT_SIZE
+      ld      e, $ff
+      call      Mem.Fill
+    _end
+
+    org     $c000
+    _data segment
+      ds      SEGMENT_SIZE, $ff
     _end
   _end
-
-  _proc Dump
-    ld      hl, segment
-    ld      bc, SEGMENT_SIZE
-    call    Writer.Dump
-    call    Writer.NewLine
-  _end
-
-  _proc Reset
-    ld      hl, segment
-    ld      bc, SEGMENT_SIZE
-    ld      e, $ff
-    call      Mem.Fill
-  _end
-
-  org     $c000
-  _data segment
-    ds      SEGMENT_SIZE, $ff
-  _end
-
-  savenex open "built/scheme/fx-page-demo.nex", Main, $bfe0
-  savenex auto
-  savenex close
-  cspectmap    "built/scheme/fx-page-demo.map"
